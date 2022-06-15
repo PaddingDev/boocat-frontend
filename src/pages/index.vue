150036b85ffa1423a51e4916867ea026dafe88c4
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-16 00:03:11 +0100

    fix: rdo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { raw } from '~/composables/getBooks'
 
-const providers = new Map<string, string>([
+const providersMap = new Map<string, string>([
   ['a', 'All'],
   ['z', 'Z-Library'],
   ['g', 'Library Genesis'],
@@ -9,6 +9,15 @@ const providers = new Map<string, string>([
   ['o', 'Open Library'],
   ['b', 'Online Books Page'],
 ])
+
+const providers = {
+  a: 'All',
+  z: 'Z-Library',
+  g: 'Library Genesis',
+  m: 'Mem of The World',
+  o: 'Open Library',
+  b: 'Online Books Page',
+}
 
 let data:raw = $ref()
 const name:string = $ref()
@@ -20,7 +29,7 @@ async function search() {
   load = true
   let provider = checked
   isMultiCol = checked === 'a'
-  if (checked === 'a') provider = Array.from(providers.keys())
+  if (checked === 'a') provider = Object.keys(providers)
   data = await getBooks(name, provider)
   load = false
 }
@@ -30,8 +39,8 @@ async function search() {
   <div class="mx-auto">
     <div class="flex-row space-x-4">
       <div v-for="(pname, index) in providers" :key="index" style="display: inline-block;">
-        <input :id="pname[0]" v-model="checked" type="radio" :value="pname[0]">
-        <label :for="pname[0]" class="mr-1">&nbsp;{{ pname[1] }}</label>
+        <input :id="index" v-model="checked" type="radio" :value="index">
+        <label :for="pname" class="mr-1">&nbsp;{{ pname }}</label>
       </div>
     </div>
 
@@ -51,15 +60,15 @@ async function search() {
   <div v-if="data" class="flex-row space-x-4">
     <template v-for="(result, prov) in data">
       <div v-if="!result.success" :key="result.err?.msg" class="flex-col space-y-4" :class="isMultiCol ? 'w-1/4' : ''">
-        <p :key="providers.get(prov)">
-          {{ providers.get(prov) }} (-1)
+        <p :key="providersMap.get(prov)">
+          {{ providersMap.get(prov) }} (-1)
         </p>
         {{ result.err?.msg }} {{ result.err?.source ? `from ${result.err?.source}` : '' }}
       </div>
       <div v-else :key="result.books![0].url" class="flex-col space-y-4" :class="isMultiCol ? 'w-1/4' : ''">
         <template v-if="isMultiCol">
-          <p :key="providers.get(prov)">
-            {{ providers.get(prov) }} ({{ result.books === undefined ? 0 : result.books.length }})
+          <p :key="providersMap.get(prov)">
+            {{ providersMap.get(prov) }} ({{ result.books === undefined ? 0 : result.books.length }})
           </p>
         </template>
         <div v-for="b in result.books" :key="b.url">
