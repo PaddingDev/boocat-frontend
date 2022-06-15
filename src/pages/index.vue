04a7195aac6634fe5f227fe07276a9c14bb96272
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-06-15 21:59:42 +0100

    feat: support title name

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup>
 import { raw } from '~/composables/getBooks'
 
-const providers = {
-  a: 'All',
-  z: 'Z-Library',
-  g: 'Library Genesis',
-  m: 'Mem of The World',
-  o: 'Open Library',
-  b: 'Online Books Page',
-}
+const providers = new Map<string, string>([
+  ['a', 'All'],
+  ['z', 'Z-Library'],
+  ['g', 'Library Genesis'],
+  ['m', 'Mem of The World'],
+  ['o', 'Open Library'],
+  ['b', 'Online Books Page'],
+])
 
 let data:raw = $ref()
 const name:string = $ref()
@@ -20,7 +20,7 @@ async function search() {
   load = true
   let provider = checked
   isMultiCol = checked === 'a'
-  if (checked === 'a') provider = Object.keys(providers)
+  if (checked === 'a') provider = Array.from(providers.keys())
   data = await getBooks(name, provider)
   load = false
 }
@@ -30,8 +30,8 @@ async function search() {
   <div class="mx-auto">
     <div class="flex-row space-x-4">
       <div v-for="(pname, index) in providers" :key="index" style="display: inline-block;">
-        <input :id="index" v-model="checked" type="radio" :value="index">
-        <label :for="pname" class="mr-1">&nbsp;{{ pname }}</label>
+        <input :id="pname[0]" v-model="checked" type="radio" :value="pname[0]">
+        <label :for="pname[0]" class="mr-1">&nbsp;{{ pname[1] }}</label>
       </div>
     </div>
 
@@ -49,11 +49,16 @@ async function search() {
   </div>
 
   <div v-if="data" class="flex-row space-x-4">
-    <template v-for="result in data">
+    <template v-for="(result, prov) in data">
       <div v-if="!result.success" :key="result.err?.msg">
         {{ result.err }}
       </div>
       <div v-else :key="result.books![0].url" class="flex-col space-y-4" :class="isMultiCol ? 'w-1/4' : ''">
+        <template v-if="isMultiCol">
+          <p :key="providers.get(prov)">
+            {{ providers.get(prov) }}
+          </p>
+        </template>
         <div v-for="b in result.books" :key="b.url">
           <div class="flex-row space-x-2">
             <span
