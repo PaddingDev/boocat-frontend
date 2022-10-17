27d03849aeb0e8a811306a419140f08e6a3c714b
Percy Ma <kecrily@gmail.com>
2022-10-17 23:25:11 +0800

    perf: a11y and SEO

--- src/components/ModeSwitch.vue
+++ src/components/ModeSwitch.vue
@@ -6,7 +6,7 @@ const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <a @click="toggleDark()">
+  <a role="button" title="Switch Color Mode" @click="toggleDark()">
     <div v-show="isDark" carbon:moon />
     <div v-show="!isDark" carbon:sun />
   </a>
