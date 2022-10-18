a65361d0c5a6dc57f7ba2e28d5872e7a0c52e82d
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2022-10-18 07:58:14 +0100

    chore: add Padding.DEV

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <footer m="x-auto y-8" text="center">
     Made with ❤️, Vue.js & .NET 6<br>
+    Proudly to be a product of <a href="https://padding.dev">Padding.DEV</a>
     Copyright &copy; BooCat 2022.&nbsp;
     <a underline href="https://boocat.org/about">About BooCat</a>
   </footer>
